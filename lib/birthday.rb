f86bcf469f5944e51da65fc6028a227f35395c34
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-10 19:46:48 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,6 @@
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    end
   end
 end
 
@@ -16,4 +15,5 @@ def age_appropriate_birthday(birthday_kids)
     if age <= 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
+  end
 end
